288d4a1311594bcfa48b33147daa01982ae6ab26
Ilya Bashilov <tango1603@yandex.ru>
2017-06-16 13:43:50 +0300

    Add models Add "Code First" for DataBase

---
+++ MyMVCProj/MyMVCProj/Models/AddressContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace AppTest.Models
+{
+    public class AddressContext : DbContext
+    {
+        public DbSet<Address> Addreses { get; set; }
+    }
+}
